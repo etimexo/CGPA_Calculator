065f89b630c4aaa46b09070de8668dd41b90a94d
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-11-26 00:48:49 +0100

    Update script.js: add units gotten, grade points, and GPAs

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-console.log("script loaded");
-let codeContainer = document.getElementById("code");
-const addBtn = document.getElementById("add-button");
-const delBtn = document.getElementById("delete-button");
-let unitsObtable = document.getElementById("units");
-let testScore = document.getElementById("test-scores");
-let examScore = document.getElementById("exam-scores");
-let grade = document.getElementById("grades");
-let gradeA = document.getElementById("grades-a");
-let gradePoint = document.getElementById("grade-points");
-let courseList = document.getElementById("course-list");
-let courseGrades = document.getElementById("course-grades");
-let unitsGotten = document.getElementById("units-gotten");
-let gpa = document.getElementById("course-grade-points");
-let courseListArr = [];
-let codeList = [];
-let unitsObtableArr = [];
-let tScoresArr = [];
-let eScoresArr = [];
-let gradeArr = [];
-let gPointArr = [];
-let gpaArr = [];
-let unitsGottenArr = [];
+console.log("script loaded")
+let codeContainer = document.getElementById("code")
+const addBtn = document.getElementById("add-button")
+const delBtn = document.getElementById("delete-button")
+let unitsObtable = document.getElementById("units")
+let testScore = document.getElementById("test-scores")
+let examScore = document.getElementById("exam-scores")
+let grade = document.getElementById("grades")
+let gradeA = document.getElementById("grades-a")
+let gradePoint = document.getElementById("grade-points")
+let courseList = document.getElementById("course-list")
+let courseGrades = document.getElementById("course-grades")
+let unitsGotten = document.getElementById("units-gotten")
+let gpa = document.getElementById("grade-points")
+let courseListArr = []
+let codeList = []
+let unitsObtableArr = []
+let tScoresArr = []
+let eScoresArr = []
+let gradeArr = []
+let gPointArr = []
+let gpaArr = []
+let unitsGottenArr = []
+let gradeAarr = []
 
 addBtn.addEventListener("click", function () {
-  let newCode = prompt("Enter Course Code");
-  let newUnit = prompt("Enter Course Units");
-  let newTestScore = prompt("Enter Test Score");
-  let newExamScore = prompt("Enter Exam Score");
+  let newCode = prompt("Enter Course Code")
+  let newUnit = prompt("Enter Course Units")
+  let newTestScore = prompt("Enter Test Score")
+  let newExamScore = prompt("Enter Exam Score")
   // let newUnits = unitsGottenArr.push
 
   // let realUnit = unitN
-  codeList.push(newCode);
-  unitsObtableArr.push(newUnit);
-  tScoresArr.push(newTestScore);
-  eScoresArr.push(newExamScore);
-  codeContainer.innerHTML = "";
-  unitsObtable.innerHTML = "";
-  testScore.innerHTML = "";
-  examScore.innerHTML = "";
+  codeList.push(newCode)
+  unitsObtableArr.push(newUnit)
+  tScoresArr.push(newTestScore)
+  eScoresArr.push(newExamScore)
+  codeContainer.innerHTML = ""
+  unitsObtable.innerHTML = ""
+  testScore.innerHTML = ""
+  examScore.innerHTML = ""
   grade.innerHTML = ""
+  gradeA.innerHTML = ""
+  unitsGotten.innerHTML = ""
+  gpa.innerHTML = ""
   
   let gradePointC = 0;
   let totalGradePointC = 0;
@@ -56,17 +60,11 @@ addBtn.addEventListener("click", function () {
     let li = document.createElement("li");
     li.innerText = unitsObtableArr[i];
     units.appendChild(li);
-    // let gradeSum = parseInt(testScore + examScore)
-    // grade.innerHTML = gradeSum
-    // let gradePointCalc = parseInt(grade * units)
-    // gradePoint = gradePointCalc
   }
   for (i = 0; i < tScoresArr.length; i++) {
     let li = document.createElement("li");
     li.innerText = tScoresArr[i];
     testScore.appendChild(li);
-    // grade.innerHTML = testScore + examScore
-    // gradePoint.innerHTML = grade * units
   }
   for (i = 0; i < eScoresArr.length; i++) {
     let li = document.createElement("li");
@@ -74,23 +72,51 @@ addBtn.addEventListener("click", function () {
     examScore.appendChild(li);
     
     if (gradeSum <= 40) {
-      gradeA.innerText = "D";
-      console.log("damn!");
+      gradeAarr.push("D")
+      unitsGottenArr.push(1)
+      gpaArr.push(unitsGottenArr[i] * unitsObtableArr[i])
+      console.log("damn!")
     } else if (gradeSum >= 41 && gradeSum <= 59) {
-      gradeA.innerText = "C";
-      console.log("average");
+      gradeAarr.push("C")
+      unitsGottenArr.push(2)
+      gpaArr.push(unitsGottenArr[i] * unitsObtableArr[i])
+      console.log("average")
     } else if (gradeSum >= 60 && gradeSum <= 69) {
-      gradeA.innerText = "B";
-      console.log("Not bad");
+      gradeAarr.push("B")
+      unitsGottenArr.push(3)
+      gpaArr.push(unitsGottenArr[i] * unitsObtableArr[i])
+      console.log("Not bad")
     } else if (gradeSum >= 70 && gradeSum <= 100) {
-      gradeA.innerText = "A";
-      console.log("Excellente");
+      gradeAarr.push("A")
+      unitsGottenArr.push(4)
+      gpaArr.push(unitsGottenArr[i] * unitsObtableArr[i])
+      console.log("Excellente")
     } 
     gradeArr.push(gradeSum);
+    // gradeAarr.push()
 
     let liGrade = document.createElement("li");
     liGrade.innerText = gradeArr[i];
     grade.appendChild(liGrade);
+
+    let liGradeA = document.createElement("li");
+    liGradeA.innerText = gradeAarr[i];
+    gradeA.appendChild(liGradeA);
+
+    let liUG = document.createElement("li");
+    liUG.innerText = unitsGottenArr[i];
+    unitsGotten.appendChild(liUG);
+
+    let liGpa = document.createElement("li");
+    liGpa.innerText = gpaArr[i];
+    gpa.appendChild(liGpa);
   }
+  // for (i = 0; i < gradeAarr.length; i++) {
+  //   let li = document.createElement("li");
+  //   li.innerText = gradeAarr[i];
+  //   gradeA.appendChild(li);
+  //   // grade.innerHTML = testScore + examScore
+  //   // gradePoint.innerHTML = grade * units
+  // }
 
 });
